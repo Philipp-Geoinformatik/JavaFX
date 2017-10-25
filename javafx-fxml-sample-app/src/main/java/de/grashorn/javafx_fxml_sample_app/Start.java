a568cea19bcf9a5c19ad77cecaa3054f83357059
gra4156 <philipp.grashorn@airbus.com>
2017-10-25 16:00:20 +0200

    hello world

---
+++ javafx-fxml-sample-app/src/main/java/de/grashorn/javafx_fxml_sample_app/Start.java
@@ -0,0 +1,11 @@
+package de.grashorn.javafx_fxml_sample_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Start {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
